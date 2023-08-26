fea2c05c2748a9aa5226e3a312d1c8b6cb4ca3ae
ifeanyi-ajimah <ajimahifeanyi@gmail>
2023-08-26 18:19:37 +0100

    meta description modified

--- resources/views/externalLayout/header.blade.php
+++ resources/views/externalLayout/header.blade.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="UTF-8">
-    <meta name="description" content="Male_Fashion Template">
-    <meta name="keywords" content="Male_Fashion, unica, creative, html">
+    <meta name="description" content="Male Fashion Aberdeen, UK - Best place to buy.">
+    <meta name="keywords" content="Male Fashion, Aberdeen Store, Best Store, ">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- CSRF Token -->
